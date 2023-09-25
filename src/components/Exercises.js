40c7148e906231d3e9ea634933c255bfedc0cb3d
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-25 13:50:00 +0100

    added exercise cards to show results

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Pagination } from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material/";
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
 
-export default Exercises
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", cs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+
+    </Box>
+  );
+};
+
+export default Exercises;
