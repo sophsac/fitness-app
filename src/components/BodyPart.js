cc54e840c21f96ba046866aea877fd9f0b0a827b
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-25 13:26:08 +0100

    added scroll package and designed exercises to scroll

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Stack, Typography } from '@mui/material';
+import React from "react";
+import { Stack, Typography } from "@mui/material";
 
-import Icon from '../assets/icons/gym.png';
+import Icon from "../assets/icons/gym.png";
 
-const BodyPart = () => {
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
   return (
-    <div>BodyPart</div>
-  )
-}
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
+      }}
+    >
+      <img
+        src={Icon}
+        alt="dumbbell"
+        style={{ width: "40px", height: "40px" }}
+      />
 
-export default BodyPart
+      <Typography
+        fontSize="20px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default BodyPart;
