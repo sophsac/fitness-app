2a95622617b6a57029dbf65a4bc62d804d118cdf
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-25 10:14:56 +0100

    details of herobanner in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
+import { Box } from '@mui/material';
 
 const Home = () => {
   return (
